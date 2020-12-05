b01e4aa0d3b3b584081e7e986858d021e7c39d47
Michael Sobrepera <mjsobrep@live.com>
2020-12-05 14:47:32 -0500

    added in a full from source build to dockerfiles

--- dockerfiles/build_images.sh
+++ dockerfiles/build_images.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-docker build -f released -t webrtc_ros/released:latest .
-docker build -f develop -t webrtc_ros/develop:latest .
-docker build -f dev_awsc14 -t webrtc_ros/dev_awsc14:latest .
+#docker build -f released -t webrtc_ros/released:latest .
+#docker build -f develop -t webrtc_ros/develop:latest .
+#docker build -f dev_awsc14 -t webrtc_ros/dev_awsc14:latest .
+
+prior=$(pwd)
+bn=$(basename "`pwd`")
+if [ "$bn" = "dockerfiles" ]
+    then
+    cd ..
+    echo "moving up to copy in files"
+elif [ "bn" = "webrtc_ros" ]
+    then
+    echo "Already in correct context"
+else
+    echo "no clue where you are: $bn"
+fi
+docker build -f dockerfiles/full_build -t webrtc_ros/develop-fullbuild:latest .
+cd $prior
